2242bf344ac4be1e930f4bcc75cc938c6dfa53fa
keffinzk12 <keffinziaulk@gmail.com>
2022-10-13 21:19:11 +0700

    new project

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+    include 'koneksi.php';
+
+    if(isset($_POST['input'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $data = mysqli_query($koneksi,"SELECT * FROM users WHERE username='".$username."' AND password='".$password."'");
+
+
+        if(mysqli_num_rows($data)){
+            echo "login successfully";
+        }else{
+            echo "wrong username/password";
+        }
+    }
+?>
--- logintest.php
+++ logintest.php
@@ -3,26 +3,25 @@
     <head>
         <meta charset="utf-8"/>
         <title>login</title>
-        <link rel="stylesheet" type="text/css" href="login style.css"
+        <link rel="stylesheet" type="text/css" href="login style.css">
     </head>
     <body>
-        <div id="login-box">
-        <div class="wrapper">
-            <h1 class="title">login from</h1>
-            <from action="#">
-                <div class="field">
-                    <input type="text" name="email" placeholder="email" id="input">
+        <h1>login</h1>
+        <div class="from">
+            <from action="login.php" method="post" name="from_input">
+                <div class="username">
+                    username : <input type="text" name="username" placeholder="username" require="">
                 </div>
-                <div class="field">
-                    <input type="password" name="password" placeholder="password" id="input">
+                <div class="password">
+                    password : <input type="password" name="password" placeholder="password" require="">
                 </div>
-                <div class="content">
+                <div class="link">
                     <div class="pass-link">
                         <a href="#">forgot password?</a>
                     </div>
                 </div>
-                <div class="field">
-                    <input type="submit" value="login" id="submit">
+                <div class="submit">
+                    <input type="submit" name="input" value="login">
                 </div>
                 <div class="signup-link">
                     don't have an account yet?<a href="sign-up.html">sign up now</a>
