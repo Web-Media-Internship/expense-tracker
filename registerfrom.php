0b8e1967aad7bd7431048fb96a87dbc38b92d546
keffinzk12 <keffinziaulk@gmail.com>
2022-10-14 07:34:11 +0700

    add 2 file

---
+++ registerfrom.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>sign_up</title>
+        <link rel="stylesheet" type="text/css" href="sign-up style.css"
+    </head>
+    <body>
+        <div id="login-box">
+        <div class="signup-box">
+            <h1> Sign-Up </h1>
+            <input type="text" name="name" placeholder="name">
+            <input type="text" name="username" placeholder="username">
+            <input type="text" name="email" placeholder="email">
+            <input type="password" name="password" placeholder="password">
+            <input type="submit" value="sign-up">
+            <div class="signup-link">
+                already have an account?<a href="logintest.php">login</a>
+            </div>
+        </div>
+        <div class="right-b">
+            <h2> WELCOME </h2>
+        </div>
+
+</html>
